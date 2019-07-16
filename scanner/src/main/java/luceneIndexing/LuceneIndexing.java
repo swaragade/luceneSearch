35f7bac29b72cd566f87616792d3f230a4f55ee5
Suraj Waragade <52372476+swaragade@users.noreply.github.com>
2019-07-16 16:56:38 +0530

    v2.0

--- scanner/src/main/java/luceneIndexing/LuceneIndexing.java
+++ scanner/src/main/java/luceneIndexing/LuceneIndexing.java
@@ -27,81 +27,85 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class LuceneIndexing {
-	public static void createIndexFromDirectory(final File inputfolder, String outputDirectory , StandardAnalyzer standardAnalyzer) throws IOException {
-		for (final File fileEntry : inputfolder.listFiles()) {
-			if (fileEntry.isDirectory()) {
-				createIndexFromDirectory(fileEntry,outputDirectory, standardAnalyzer);
-			} else {
-				//System.out.println(fileEntry.getCanonicalFile());
-				//System.out.println(fileEntry.getName());
-				
-				String inputFilePath = fileEntry.getCanonicalFile().toString();
-				File file = new File(inputFilePath);
+	public static void createIndexFromDirectory(final File inputfolder, String outputDirectory,
+			StandardAnalyzer standardAnalyzer) throws IOException {
+		if (null != inputfolder && null != outputDirectory && inputfolder.listFiles() != null
+				&& inputfolder.listFiles().length > 0) {
+			for (final File fileEntry : inputfolder.listFiles()) {
+				if (fileEntry.isDirectory()) {
+					createIndexFromDirectory(fileEntry, outputDirectory, standardAnalyzer);
+				} else {
+					
+					// System.out.println(fileEntry.getCanonicalFile());
+					// System.out.println(fileEntry.getName());
 
-				Directory directory = FSDirectory.open(Paths.get(outputDirectory));
-				IndexWriterConfig config = new IndexWriterConfig(standardAnalyzer);
-				config.setOpenMode(OpenMode.CREATE); 
+					String inputFilePath = fileEntry.getCanonicalFile().toString();
+					if (inputFilePath.endsWith(".java") || inputFilePath.endsWith(".xml")
+							|| inputFilePath.endsWith(".wsdl") || inputFilePath.endsWith(".xsd")
+							|| inputFilePath.endsWith(".properties")) {
+						File file = new File(inputFilePath);
 
-				IndexWriter writer = new IndexWriter(directory, config);
+						Directory directory = FSDirectory.open(Paths.get(outputDirectory));
+						IndexWriterConfig config = new IndexWriterConfig(standardAnalyzer);
+						config.setOpenMode(OpenMode.CREATE);
 
-				Document document = new Document();
-				//index for path
-				document.add(new StringField("path", file.getPath(), Field.Store.YES));
-				//index for filename
-				//document.add(new StringField("filename", file.getName(), Field.Store.YES));
-				try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-					//index for content
-					document.add(new TextField("content", br));
-					writer.addDocument(document);
-					writer.close();
+						IndexWriter writer = new IndexWriter(directory, config);
 
-				} catch (IOException e) {
-					e.printStackTrace();
+						Document document = new Document();
+						// index for path
+						document.add(new StringField("path", file.getPath(), Field.Store.YES));
+						// index for filename
+						// document.add(new StringField("filename", file.getName(), Field.Store.YES));
+						try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+							// index for content
+							document.add(new TextField("content", br));
+							writer.addDocument(document);
+							writer.close();
+
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
 				}
-				
 			}
 		}
 	}
 
-	
-	public static void searchAndLog(String outputDirectory, StandardAnalyzer standardAnalyzer) throws IOException, ParseException {
+	public static void searchAndLog(String outputDirectory, StandardAnalyzer standardAnalyzer)
+			throws IOException, ParseException {
 		Directory directory = FSDirectory.open(Paths.get(outputDirectory));
 		IndexReader reader = DirectoryReader.open(directory);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        QueryParser parser = new QueryParser("content", standardAnalyzer);
-        Query query = parser.parse("Chicago");
-        TopDocs results = searcher.search(query, 100);
-        System.out.println("Hits for search -->" + results.totalHits);
-        
-        for (ScoreDoc scoreDoc : results.scoreDocs) {
-        	Document dtemp = searcher.doc(scoreDoc.doc);
-        	dtemp.getFields();
-        	 for (Object o : dtemp.getFields()) {
-        	        
-        	        Field field = (Field) o;
-        	        //System.out.println("Field has no name  :"+field.name());
-        	        System.out.println("Search found in this file : "+
-        	                field.stringValue());
-        	    }
-        	//resultDocs.add(searcher.doc(scoreDoc.doc));
-        }
-        //System.out.println("last :: "+resultDocs.get(0).get("content"));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		QueryParser parser = new QueryParser("content", standardAnalyzer);
+		Query query = parser.parse("void");
+		TopDocs results = searcher.search(query, 100);
+		System.out.println("Hits for search -->" + results.totalHits);
+
+		for (ScoreDoc scoreDoc : results.scoreDocs) {
+			Document dtemp = searcher.doc(scoreDoc.doc);
+			dtemp.getFields();
+			for (Object o : dtemp.getFields()) {
+
+				Field field = (Field) o;
+				// System.out.println("Field has no name :"+field.name());
+				System.out.println("Search found in this file : " + field.stringValue());
+			}
+			// resultDocs.add(searcher.doc(scoreDoc.doc));
+		}
+		// System.out.println("last :: "+resultDocs.get(0).get("content"));
 	}
-	
-	
-	
-	
+
 	public static void main(String[] args) throws IOException, ParseException {
 		long startTime = Calendar.getInstance().getTimeInMillis();
-		long endTime;
-		final File inputfolder = new File("E:\\Lucene\\sample_directory_for_testing\\");
-		String outputDirectory = "E:\\Lucene\\temp_test_index\\";
+		final File inputfolder = new File("E:\\Lucene");
+
+		String outputDirectory = "E:\\Lucene\\temp_test_index\\new";
 		StandardAnalyzer standardAnalyzer = new StandardAnalyzer();
-		createIndexFromDirectory(inputfolder,outputDirectory, standardAnalyzer);
-		endTime = Calendar.getInstance().getTimeInMillis();
-		System.out.println("Time taken for indexing : "+(endTime-startTime)+" ms");
+		createIndexFromDirectory(inputfolder, outputDirectory, standardAnalyzer);
+		long midTime = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Time taken for indexing : " + (midTime - startTime) + " ms");
 		searchAndLog(outputDirectory, standardAnalyzer);
-		endTime = Calendar.getInstance().getTimeInMillis();
-		System.out.println("Total time taken for search : "+(endTime-startTime)+" ms");
+		long endTime = Calendar.getInstance().getTimeInMillis();
+		System.out.println("Total time taken for search : " + (endTime - midTime) + " ms");
 	}
 }
